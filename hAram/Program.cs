bf63bfa122189882ef0271b6fd1a5eb6df3ce789
gkrdl <p1442cs9@gmail.com>
2015-03-03 21:41:32 +0900

    [WIP] hAram
    Project Create

---
+++ hAram/Program.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeagueSharp;
+using LeagueSharp.Common;
+using SharpDX;
+
+namespace hAram
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            CustomEvents.Game.OnGameLoad += Game_OnGameLoad;
+        }
+
+        private static void Game_OnGameLoad(EventArgs args)
+        {
+            var type = Type.GetType("hAram.Champions." + ObjectManager.Player.ChampionName);
+
+            Activator.CreateInstance(type);
+        }
+    }
+}
